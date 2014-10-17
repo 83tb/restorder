348e8868c7d7c5d937cb7854570b8e15ede8f716
kuba kucharski <kuba@kucharski.it>
2014-10-17 17:43:45 +0200

    full install script

--- install.sh
+++ install.sh
@@ -5,31 +5,26 @@
 
 
 
-FROM ubuntu:trusty
-MAINTAINER Helmi <helmi@tuxuri.com>
 
-RUN echo "deb http://archive.ubuntu.com/ubuntu trusty main universe" > /etc/apt/sources.list
-RUN apt-get -y update
-RUN apt-get -y install ca-certificates
-RUN apt-get -y install wget
-RUN wget --quiet --no-check-certificate -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-RUN echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" >> /etc/apt/sources.list
-RUN apt-get -y update
-RUN apt-get -y upgrade
-RUN locale-gen --no-purge en_US.UTF-8
-ENV LC_ALL en_US.UTF-8
-RUN update-locale LANG=en_US.UTF-8
-RUN apt-get -y install postgresql-9.3 postgresql-contrib-9.3 postgresql-9.3-postgis-2.1 postgis
-RUN echo "host    all             all             0.0.0.0/0               md5" >> /etc/postgresql/9.3/main/pg_hba.conf
-RUN service postgresql start && /bin/su postgres -c "createuser -d -s -r -l docker" && /bin/su postgres -c "psql postgres -c \"ALTER USER docker WITH ENCRYPTED PASSWORD 'docker'\"" && service postgresql stop
-RUN echo "listen_addresses = '*'" >> /etc/postgresql/9.3/main/postgresql.conf
-RUN echo "port = 5432" >> /etc/postgresql/9.3/main/postgresql.conf
+#RUN echo "deb http://archive.ubuntu.com/ubuntu trusty main universe" > /etc/apt/sources.list
+apt-get -y update
+apt-get -y install ca-certificates
+apt-get -y install wget
+wget --quiet --no-check-certificate -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" >> /etc/apt/sources.list
+apt-get -y update
+apt-get -y upgrade
+locale-gen --no-purge en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+update-locale LANG=en_US.UTF-8
+apt-get -y install postgresql-9.3 postgresql-contrib-9.3 postgresql-9.3-postgis-2.1 postgis
+echo "host    all             all             0.0.0.0/0               md5" >> /etc/postgresql/9.3/main/pg_hba.conf
+service postgresql start && /bin/su postgres -c "createuser -d -s -r -l docker" && /bin/su postgres -c "psql postgres -c \"ALTER USER docker WITH ENCRYPTED PASSWORD 'docker'\"" && service postgresql stop
+echo "listen_addresses = '*'" >> /etc/postgresql/9.3/main/postgresql.conf
+echo "port = 5432" >> /etc/postgresql/9.3/main/postgresql.conf
 
-EXPOSE 5432
-
-ADD start.sh /start.sh
-RUN chmod 0755 /start.sh
-
-CMD ["/start.sh"]
+apt-get install libpq-dev python-dev
+service postgresql start
+/bin/su postgres -c "createdb powermanager" 
 
 
