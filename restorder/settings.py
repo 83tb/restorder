e7e5e5592c6bc00c7213330ed2a7ca05562dc00d
Kuba Kucharski <kuba@kucharski.it>
2015-01-26 16:40:46 +0100

    fix in db address 2

--- restorder/settings.py
+++ restorder/settings.py
@@ -67,7 +67,7 @@ WSGI_APPLICATION = 'restorder.wsgi.application'
 DATABASES = {
     'default': {
 
-	#'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+	'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'powermanager',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'docker',
