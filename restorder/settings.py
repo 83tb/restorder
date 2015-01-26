7cec3adf77d44ffa087a1b974791de7c7ace0d4b
Kuba Kucharski <kuba@kucharski.it>
2015-01-26 16:40:22 +0100

    fix in db address

--- restorder/settings.py
+++ restorder/settings.py
@@ -68,18 +68,18 @@ DATABASES = {
     'default': {
 
 	#'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        #'NAME': 'powermanager',                      # Or path to database file if using sqlite3.
+        'NAME': 'powermanager',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        #'USER': 'docker',
-        #'PASSWORD': 'docker',
-        #'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-        #'PORT': '',                      # Set to empty string for default.
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'docker',
         'USER': 'docker',
-        'HOST': 'db',
-        'PORT': 5432,
+        'PASSWORD': 'docker',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+
+        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        ##'NAME': 'docker',
+        #'USER': 'docker',
+        #'HOST': 'db',
+        #'PORT': 5432,
 
 
 
