d4f4b94f7e5d57cce215a678b1671d3aa9c8b6ed
Kuba Kucharski <kuba@kucharski.it>
2015-04-12 21:21:10 +0200

    no urls

--- restorder/urls.py
+++ restorder/urls.py
@@ -10,7 +10,7 @@ router.register(r'ars', views.AreaViewSet)
 router.register(r'lamp', views.LampViewSet)
 router.register(r'volume', views.VolumeViewSet)
 router.register(r'lgroup', views.LGroupViewSet)
-router.register(r'lamploc', views.LampList.as_view())
+router.register(r'lamploc', views.LampList.as_view(), base_name="lamps")
 
 
 
